ed1dc005270545f29394fef7db8be8a6c7ec9056
haruya46 <pazu70@gmail.com>
2023-08-02 23:19:38 +0900

    2023/8/2/deployする

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('post', PostController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
