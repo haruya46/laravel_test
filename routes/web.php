5e3930afd852861f3d8811844382a6961fd0b390
kitabayasi kano <hiraseiya0128@icloud.com>
2023-08-08 23:24:32 +0900

    2023/08/08/別端末から変更点を更新

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/index', [GuestController::class, 'index'])->name('guest.index');
 
 
 Route::middleware('auth')->group(function () {
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
